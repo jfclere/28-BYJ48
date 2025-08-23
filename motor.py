68afc397816f40fce811312d966c77e72847667e
Jean-Frederic Clere <jfclere@gmail.com>
2025-08-23 15:26:33 +0200

    Arrange motor.py into a class.

--- motor.py
+++ motor.py
@@ -16,17 +16,11 @@ import time
 # Pink IN3
 # Blue IN4
 
-IN1 = 23
-IN2 = 24
-IN3 = 25
-IN4 = 8
-
-# Set GPIO mode and configure pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.setup(IN4, GPIO.OUT)
+# Like
+#IN1 = 23
+#IN2 = 24
+#IN3 = 25
+#IN4 = 8
 
 # Define constants
 # the specs say:
@@ -57,8 +51,23 @@ se4 = [
     [1, 0, 0, 1]
 ]
 
+class motor():
+  def __init__(self, IN1 = 23, IN2 = 24, IN3 = 25, IN4 = 8):
+    self.IN1 = IN1
+    self.IN2 = IN2
+    self.IN3 = IN3
+    self.IN4 = IN4
+
+    # Set GPIO mode and configure pins
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.IN1, GPIO.OUT)
+    GPIO.setup(self.IN2, GPIO.OUT)
+    GPIO.setup(self.IN3, GPIO.OUT)
+    GPIO.setup(self.IN4, GPIO.OUT)
+
+
 # Function to rotate the stepper motor one step
-def step(delay, step_sequence):
+  def step(delay, step_sequence):
     GPIO.output(IN1, step_sequence[0])
     GPIO.output(IN2, step_sequence[1])
     GPIO.output(IN3, step_sequence[2])
@@ -70,14 +79,14 @@ def step(delay, step_sequence):
     GPIO.output(IN4, 0)
 
 # Functions to move the stepper motor one step forward
-def step_forward(delay, steps):
+  def step_forward(delay, steps):
     for _ in range(steps):
         step(delay, se4[0])
         step(delay, se4[1])
         step(delay, se4[2])
         step(delay, se4[3])
 
-def step_forward8(delay, steps):
+  def step_forward8(delay, steps):
     for _ in range(steps):
         step(delay, se8[0])
         step(delay, se8[1])
@@ -89,13 +98,13 @@ def step_forward8(delay, steps):
         step(delay, se8[7])
 
 # Functions to move the stepper motor one step backward
-def step_backward(delay, steps):
+  def step_backward(delay, steps):
     for _ in range(steps):
         step(delay, se4[3])
         step(delay, se4[2])
         step(delay, se4[1])
         step(delay, se4[0])
-def step_backward8(delay, steps):
+  def step_backward8(delay, steps):
     for _ in range(steps):
         step(delay, se8[7])
         step(delay, se8[6])
@@ -105,7 +114,7 @@ def step_backward8(delay, steps):
         step(delay, se8[2])
         step(delay, se8[1])
 
-def reset():
+  def reset():
      delay = 0.005
      for _ in range(12):
         step_backward8(delay, STEPS_PER_REVOLUTION)
@@ -116,21 +125,21 @@ if __name__ == "__main__":
         delay = 0.005
         # delay = 1
 
+        mymotor = motor(IN1 = 23, IN2 = 24, IN3 = 25, IN4 = 8)
         print(se4)
         print(STEPS_PER_REVOLUTION)
-        step_forward8(delay, 1)
-        exit(0)
+        mymotor.step_forward8(delay, 1)
 
         while True:
             # Rotate one revolution forward (clockwise)
-            step_forward8(delay, STEPS_PER_REVOLUTION)
+            mymotor.step_forward8(delay, STEPS_PER_REVOLUTION)
             # step_backward(delay, STEPS_PER_REVOLUTION)
 
             # Pause for 2 seconds
             time.sleep(2)
 
             # Rotate one revolution backward (anticlockwise)
-            step_backward8(delay, STEPS_PER_REVOLUTION)
+            mymotor.step_backward8(delay, STEPS_PER_REVOLUTION)
 
             # Pause for 2 seconds
             # time.sleep(2)
